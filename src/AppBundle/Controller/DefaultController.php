532bcd28851ae74ce5a53f7126e135ad0b9835d1
Weiwei OUYANG <i@wouyang.com>
2016-05-18 17:55:05 +0200

    add page procure and QRcode png

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -11,15 +11,23 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         return $this->render('default/index.html.twig', array());
     }
 
     /**
+     * @Route("/procure", name="procure")
+     */
+    public function procureAction()
+    {
+        return $this->render('default/procure.html.twig', array());
+    }
+
+    /**
      * @Route("/symfony", name="symfony_homepage")
      */
-    public function symfonyAction(Request $request)
+    public function symfonyAction()
     {
         // replace this example code with whatever you need
         return $this->render('default/symfony.html.twig', array(
